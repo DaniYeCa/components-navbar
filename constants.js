772b117c7c270fc91ccfa184f062ba373cf46eb3
daniyeca <daniel.dance1912@gmail.com>
2023-07-02 17:39:28 +0200

    finish my navbar componet

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const DOCUMENTATION = [
+  {
+    title: 'Lo mejor de React',
+    url: '',
+    favorite: true,
+  },
+  {
+    title: 'Documnetacionde REACT',
+    url: '',
+    favorite: false,
+  },
+  {
+    title: 'Documentacion de JS',
+    url: '',
+    favorite: true,
+  },
+  {
+    title: 'Ejemplos de CSS',
+    url: '',
+    favorite: false,
+  },
+]
